b500d26e99bba10bbf5bbbc491906fcb5d8262f8
Timothe Jost <timothe.jost@wanadoo.fr>
2024-03-20 17:44:47 +0100

    fix issue with circular import

--- suite2p/__init__.py
+++ suite2p/__init__.py
@@ -2,7 +2,7 @@
 Copyright © 2023 Howard Hughes Medical Institute, Authored by Carsen Stringer and Marius Pachitariu.
 """
 
-__version__ = "0.14.6"
+__version__ = "0.14.7"
 
 from .default_ops import default_ops
 from .run_s2p import run_s2p, run_plane, pipeline
