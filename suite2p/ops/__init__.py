e2b8696d994cfbad7bcba25bf471cabe319f38bc
Timothe Jost <timothe.jost@wanadoo.fr>
2025-06-30 18:37:43 +0200

    Enables pickle in ops saving
    Allows saving operations with pickle enabled to support more complex data structures. This change ensures compatibility with numpy's save function where data may require serialization.

--- suite2p/ops/__init__.py
+++ suite2p/ops/__init__.py
@@ -8,4 +8,4 @@ def update_ops(ops: dict, **kwargs):
 
 def save_ops(ops: dict):
     if ops.get("ops_path"):
-        np.save(ops["ops_path"], ops)
+        np.save(ops["ops_path"], ops, allow_pickle=True)
